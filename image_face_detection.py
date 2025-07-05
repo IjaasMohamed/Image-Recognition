a90759b7094408b800937948d70c05fc22886415
Ijaas Fouzer <undyin0@gmail.com>
2025-07-05 20:12:12 +0530

    split and sort

--- image_face_detection.py
+++ image_face_detection.py
@@ -13,3 +13,7 @@ image_to_detect = cv2.imread('images/trump-modhi.png')
 all_face_locations = face_recognition.face_locations(image_to_detect, model='hog')
 
 print('There are {} no of faces in this image'.format(len(all_face_locations)))
+
+for index, current_face_location in enumerate(all_face_locations):
+    top_pos,right_pos,bottom_pos,left_pos = current_face_location
+    print("Found face {} at top:{}, right:{}, bottom:{}, left:{}".format(index+1, top_pos,right_pos,bottom_pos,left_pos))
