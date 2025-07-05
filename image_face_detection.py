4c8f2e1b2441ad9fdb8d68286c5630694c149319
Ijaas Fouzer <undyin0@gmail.com>
2025-07-05 20:16:57 +0530

    detect sort split and view

--- image_face_detection.py
+++ image_face_detection.py
@@ -17,3 +17,5 @@ print('There are {} no of faces in this image'.format(len(all_face_locations)))
 for index, current_face_location in enumerate(all_face_locations):
     top_pos,right_pos,bottom_pos,left_pos = current_face_location
     print("Found face {} at top:{}, right:{}, bottom:{}, left:{}".format(index+1, top_pos,right_pos,bottom_pos,left_pos))
+    current_face_image = image_to_detect[top_pos:bottom_pos,left_pos:right_pos]
+    cv2.imshow("Face No "+ str(index+1),current_face_image)
