e2b13ef46810c8e39b0e077b61ac89c14da10f7b
Ijaas <undyin0@gmail.com>
2025-07-06 17:13:36 +0530

    Capture

--- realtime_face_detection.py
+++ realtime_face_detection.py
@@ -5,3 +5,14 @@ Created on Sun Jul  6 16:39:07 2025
 @author: acer
 """
 
+import cv2
+import face_recognition
+
+webcam_vedio_stream = cv2.VideoCapture(0)
+
+all_face_locations = []
+
+while True:
+    ret, current_frame = webcam_vedio_stream.read()
+    current_frame_small = cv2.resize(current_frame, (0,0),fx=0.25, fy=0.25) #resize to speed
+    all_face_locations = face_recognition.face_locations(image_to_detect, model='hog')
